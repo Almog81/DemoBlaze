1763457083efbcc53730e21772bfd9b09fb92ec8
Almog81 <almog81@gmail.com>
2024-11-24 13:52:06 +0200

    Start Project

---
+++ src/main/java/PageObjects/RegisterPage.java
@@ -0,0 +1,11 @@
+package PageObjects;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class RegisterPage {
+    @FindBy(css = "show > .d-none")
+    public WebElement show_myAccount;
+
+    //TODO elements
+}
